b35169917d0595b4e9dfa54b0cc65cbaccff8026
Lasantha Mudalige <lasantha1130@gmail.com>
2022-08-10 20:29:56 +0530

    complete file

--- main.py
+++ main.py
@@ -1,5 +1,78 @@
+morse_chart = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+    "!": "-.-.--",
+    "@": ".--.-.",
+    "$": "...-..-",
+    "%": "-..-.",
+    "&": ".-...",
+    "'": ".----.",
+    ")": "-.--.-",
+    "(": "-.--.",
+    ";": "---...",
+    ",": "--..--",
+    "=": "-...-",
+    ".": ".-.-.-",
+    "-": "-....-",
+    "+": ".-.-.",
+    '"': ".-..-.",
+    "?": "..--..",
+    "/": "-..-."
+}
+
+
 def main():
-    text = input("Enter text to convert to morse code: ")
+    text = input("Enter text to convert to morse code: ").upper()
+
+    morse_code = ""
+
+    for letter in text:
+        # if letter is a space the add '/' to it
+        if letter == " ":
+            morse_code += "/"
+        # else get letter from the morse chart
+        else:
+            morse_letter = morse_chart[letter]
+            morse_code += morse_letter
+
+        # add space after every letter
+        morse_code += " "
+
+    print("Your morse code is", morse_code)
 
 
 main()
